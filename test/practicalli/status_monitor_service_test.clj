f4c5347bfe4b7a4ca31ad9d79f2932745b3af702
Jaret Binford <jaretbinford@gmail.com>
2020-10-12 23:02:23 -0400

    Fixing minor things

--- test/practicalli/status_monitor_service_test.clj
+++ test/practicalli/status_monitor_service_test.clj
@@ -1,6 +1,5 @@
 (ns practicalli.status-monitor-service-test
-  (:require [clojure.test :refer :all]
-            [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest is testing]]
             [ring.mock.request :as mock]
             [practicalli.status-monitor-service :as SUT]))
 
@@ -12,18 +11,4 @@
             :body    "Status Monitor Dashboard via CircleCI and Heroku"
             :headers {}}))))
 
-(defn check-ip-handler [request]
-  (ring.util.response/content-type
-    (ring.util.response/response (:remote-addr request))
-    "text/plain"))
 
-(comment
-(ring.util.response/bad-request "Hello")
-(ring.util.response/created "/post/clojure-is-awesome")
-(ring.util.response/redirect "https://clojure.org/getting-started/")
-(ring.util.response/status (ring.util.response/response "Time for Cake!") 555)
-(ring.util.response/content-type (ring.util.response/response "Hello") "text/plain")
-(ring.util.response/header (ring.util.response/response "Hello") "X-Tutorial-For" "Baeldung")
-(ring.util.response/set-cookie (ring.util.response/response "Hello") "User" "123")
-(check-ip-handler (ring.util.response/bad-request "Hello"))
-)

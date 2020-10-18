705e97264982af34a143eab5f23dd21da756bb6e
Jaret Binford <jaretbinford@gmail.com>
2020-10-17 21:23:43 -0400

    dog pic

--- src/practicalli/request_handlers.clj
+++ src/practicalli/request_handlers.clj
@@ -25,10 +25,10 @@
          (link-to {:class "button is-danger"}  "/register" "Register")
          [:p {:class "content"}
           "Manage your money without unexpected side-effects using a simple made easy banking service"]
-         [:img {:src "mafiadog.jpg"}]]]
+         [:img {:src "https://github.com/Jaretbinford/status-monitor-service/blob/master/src/mafiadog.jpg"}]]]
        ])))
 
 (def scoreboard
   [:div
    [:h1 "Mafia Scoreboard!"]
-   [:img {:src "mafiadog.jpg"} "A sunny seaside view"]])
+   [:img {:src "https://github.com/Jaretbinford/status-monitor-service/blob/master/src/mafiadog.jpg"} "Da crew!"]])
